c8422189363d1575ecaa7e1007c3f90ed8de12c3
AnamikaSa <amanika0061@gmail.com>
2022-11-09 20:38:41 +0600

    added for reviews data

--- index.js
+++ index.js
@@ -13,13 +13,33 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bu444bw.mongodb.net/?retryWrites=true&w=majority`;
 
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const servicesCollection=client.db('service_review').collection('services');
+        const reviewCollection=client.db('service_review').collection('reviews');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // reviews
+        app.post('/reviews', async (req, res) => {
+            const r = req.body;
+            const result = await reviewCollection.insertOne(r);
+            res.send(result);
+        });
+    }
+    finally{
+
+    }
+
+}
+run().catch(er=>console.error(er));
 
 
 app.get('/',(req,res)=>{
